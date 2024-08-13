25d910260aa6930d7cb198b8da7b63e2c2b998ce
MATHEUS AUGUSTO DOS SANTOS <00001120132745sp@aluno.educacao.sp.gov.br>
2024-08-12 21:06:08 -0300

    ex01

---
+++ ex03.py
@@ -0,0 +1,6 @@
+numero1 = int(input("digite o 1º numero: "))
+numero2 = int(input("digite o 2º numero: "))
+numero3 = int(input("digite o 3º numero: "))
+
+resultado  = numero1+numero2+numero3
+print("")
